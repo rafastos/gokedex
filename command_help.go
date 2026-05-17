c6e7263f7e821585085abcad312800aa00935457
Rafael S Lima <rafaelkra2@gmail.com>
2026-05-17 17:33:25 -0300

    Update command functions to accept variable arguments for improved flexibility

--- command_help.go
+++ command_help.go
@@ -2,7 +2,7 @@ package main
 
 import "fmt"
 
-func commandHelp(c *config) error {
+func commandHelp(c *config, args ...string) error {
 	fmt.Println("\nWelcome to the Pokedex!")
 	fmt.Printf("Usage:\n\n")
 	for _, cmd := range getCommands() {
