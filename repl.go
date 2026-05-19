0e91e80316720d9cfc0b0163b94209e5545408fa
Rafael S Lima <rafaelkra2@gmail.com>
2026-05-18 22:46:02 -0300

    Add catch command to allow users to attempt catching a pokemon

--- repl.go
+++ repl.go
@@ -13,6 +13,7 @@ type config struct {
 	pokeapiClient    pokeapi.Client
 	nextLocationsURL *string
 	prevLocationsURL *string
+	caughtPokemon    map[string]pokeapi.Pokemon
 }
 
 func startRepl(cfg *config) {
@@ -69,6 +70,11 @@ func getCommands() map[string]cliCommand {
 			name:        "explore <location_name>",
 			description: "Explore a location",
 			callback:    commandExplore,
+		},
+		"catch": {
+			name:        "catch <pokemon_name>",
+			description: "Attempt to catch a pokemon",
+			callback:    commandCatch,
 		},
 		"map": {
 			name:        "map",
